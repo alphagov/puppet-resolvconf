0e72223dbf87d6e84d38688a5aba0e84257d8a1c
Dan Carley <dan.carley@gmail.com>
2013-07-01 16:19:18 +0100

    [#52117923] Match entire template content in tests
    Use `\A` and `\Z` escape characters to match the entire contents of the
    template string. A trailing newline at EOF can be ignored. This ensures that
    only the two comment lines and the conditional lines appear in each context.
    
    Added an additional test for param defaults.

--- spec/classes/resolvconf_config_spec.rb
+++ spec/classes/resolvconf_config_spec.rb
@@ -2,38 +2,47 @@ require 'spec_helper'
 
 describe 'resolvconf::config', :type => :class do
   let(:file_path) { '/etc/resolvconf/resolv.conf.d/head' }
+  let(:file_header) { /\A(#[^\n]*\n){2}/ }
+
+  context 'params defaults' do
+    let(:params) {{ }}
+
+    it { should contain_file(file_path).with_content(/#{file_header}\Z/) }
+  end
 
   context 'use_local' do
-    let(:resolv_content) {
-      /YOUR CHANGES WILL BE OVERWRITTEN\nnameserver 127.0.0.1$/
-    }
-
-    context 'false (default)' do
+    context 'false (default) with nameservers' do
       let(:params) {{
-        :nameservers => ['1.1.1.1', '2.2.2.2'],
+        :nameservers => ['1.1.1.1'],
       }}
 
-      it { should_not contain_file(file_path).with_content(resolv_content) }
+      it { should contain_file(file_path).with_content(
+        /#{file_header}nameserver 1\.1\.1\.1\Z/
+      )}
     end
 
-    context 'true, with nameservers' do
+    context 'true with nameservers' do
       let(:params) {{
         :use_local   => true,
-        :nameservers => ['1.1.1.1', '2.2.2.2'],
+        :nameservers => ['1.1.1.1'],
       }}
 
-      it { should contain_file(file_path).with_content(resolv_content) }
+      it { should contain_file(file_path).with_content(
+        /#{file_header}nameserver 127\.0\.0\.1\nnameserver 1\.1\.1\.1\Z/
+      )}
     end
 
-    context 'true, without nameservers' do
+    context 'true without nameservers' do
       let(:params) {{
         :use_local => true,
       }}
 
-      it { should contain_file(file_path).with_content(resolv_content) }
+      it { should contain_file(file_path).with_content(
+        /#{file_header}nameserver 127\.0\.0\.1\Z/
+      )}
     end
 
-    context 'true, with dhcp_enabled' do
+    context 'true, even with dhcp_enabled' do
       let(:facts) {{
         :dhcp_enabled => 'true',
       }}
@@ -41,7 +50,9 @@ describe 'resolvconf::config', :type => :class do
         :use_local => true,
       }}
 
-      it { should contain_file(file_path).with_content(resolv_content) }
+      it { should contain_file(file_path).with_content(
+        /#{file_header}nameserver 127\.0\.0\.1\Z/
+      )}
     end
   end
 
@@ -52,33 +63,35 @@ describe 'resolvconf::config', :type => :class do
 
     context 'override_dhcp false (default)' do
       let(:params) {{
-        :nameservers => ['1.1.1.1', '2.2.2.2'],
+        :nameservers => ['1.1.1.1'],
       }}
 
-      it { should_not contain_file(file_path).with_content(/^nameserver/) }
+      it { should contain_file(file_path).with_content(
+        /#{file_header}\Z/
+      )}
     end
 
     context 'override_dhcp true' do
       let(:params) {{
         :override_dhcp => true,
-        :nameservers   => ['1.1.1.1', '2.2.2.2'],
+        :nameservers   => ['1.1.1.1'],
       }}
 
-      it {
-        should contain_file(file_path).with_content(
-          /^nameserver 1.1.1.1\nnameserver 2.2.2.2$/
-        )
-      }
+      it { should contain_file(file_path).with_content(
+        /#{file_header}nameserver 1\.1\.1\.1\Z/
+      )}
     end
   end
 
   context 'dhcp_enabled false' do
-    let(:facts) {{ :dhcp_enabled => 'false' }}
+    let(:facts) {{
+      :dhcp_enabled => 'false',
+    }}
 
     context 'nameservers => []' do
       let(:params) {{ :nameservers => [] }}
 
-      it { should_not contain_file(file_path).with_content(/^nameserver/) }
+      it { should contain_file(file_path).with_content(/#{file_header}\Z/) }
     end
 
     [
@@ -88,7 +101,9 @@ describe 'resolvconf::config', :type => :class do
       context "nameservers => #{param.inspect}" do
         let(:params) {{ :nameservers => param }}
 
-        it { should contain_file(file_path).with_content(/^nameserver 1.1.1.1$/) }
+        it { should contain_file(file_path).with_content(
+          /#{file_header}nameserver 1\.1\.1\.1\Z/
+        )}
       end
     end
 
@@ -99,11 +114,9 @@ describe 'resolvconf::config', :type => :class do
       context "nameservers => #{param.inspect}" do
         let(:params) {{ :nameservers => param }}
 
-        it {
-          should contain_file(file_path).with_content(
-            /^nameserver 1.1.1.1\nnameserver 2.2.2.2$/
-          )
-        }
+        it { should contain_file(file_path).with_content(
+          /#{file_header}nameserver 1\.1\.1\.1\nnameserver 2\.2\.2\.2\Z/
+        )}
       end
     end
   end
